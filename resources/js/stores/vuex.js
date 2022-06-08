13a30a16c321657f97d6e3ced0495fb80a0cf117
kakapo062 <61102115+kakapo062@users.noreply.github.com>
2022-06-08 18:19:45 +0900

    vuexを道入。プレビュー表示完了。

---
+++ resources/js/stores/vuex.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+ 
+Vue.use(Vuex)
+ 
+export default new Vuex.Store({
+  state: {
+    resume:{}
+  },
+  mutations: {
+    setResume(state, userDetail) {
+      state.resume = {...state.resume, ...userDetail}
+  }, //スプレット構文を用いてオブジェクトを展開して結合させる。
+  },
+  getters: {
+  },
+  actions: {
+    setResume({commit}, resume) {
+      commit("setResume", resume);
+  }, //スプレット構文を用いてオブジェクトを展開して結合させる。
+  }
+})
