f1f435f6fe1986bedc230153c8784312a154c8fd
kakapo062 <61102115+kakapo062@users.noreply.github.com>
2022-07-27 19:46:26 +0900

    メール添付機能実装完了

--- app/Http/Controllers/PdfOutputController.php
+++ app/Http/Controllers/PdfOutputController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;  // ファサードを読み込み
+use Illuminate\Support\Facades\Storage;
 
 class PdfOutputController extends Controller
 {
@@ -16,16 +17,19 @@ class PdfOutputController extends Controller
         $email = $attributes['submit_email'];
         $pdf = \PDF::loadView('pdf_output', compact('attributes','licenses', 'workHistories', 'schoolHistories'))
         ->setPaper('A4')
-        ->setOption('encoding', 'utf-8');
+        ->setOption('encoding', 'utf-8')
+        ->save(storage_path('app/pdf/履歴書.pdf'));
 
         Mail::send('emails.text',$attributes, function($attributes) use ($email) {
             $attributes
                 ->to($email)
+                ->attach(storage_path('app/pdf/履歴書.pdf'))
                 ->subject('履歴書データの送付');
         });
-        $pdf->download('履歴書.pdf');  //returnがないとダウンロードされない
-        // return $pdf->download('履歴書.pdf');  //ダウンロード
+        Storage::delete('pdf/履歴書.pdf');
         return redirect('/start/resume/thanks'); //returnは送信処理完了後に表示するページや内容を指定する。
+
+        // return $pdf->download('履歴書.pdf');  //ダウンロード
     }
 
     public function download(Request $request) {
